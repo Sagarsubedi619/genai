918daeb5f6327ebcabab96cb76600c10f4471e73
sagar.subedi <sagarsubedi619@gmail.com>
2024-10-04 19:59:11 -0500

    Added application properties for ollama model

--- src/main/java/com/explore/genai/managers/ChatManagerImpl.java
+++ src/main/java/com/explore/genai/managers/ChatManagerImpl.java
@@ -1,20 +1,18 @@
 package com.explore.genai.managers;
 
-import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ChatManagerImpl implements ChatManager
 {
 	@Autowired
-	private ChatClient chatClient;
+	private OllamaChatModel ollamaChatModel;
 
 	@Override
 	public String handlePrompt(String prompt)
 	{
-		return chatClient.prompt()
-				.user(prompt)
-				.call()
-				.chatResponse()
-				.toString();
+		return ollamaChatModel.call(prompt);
 	}
 }
